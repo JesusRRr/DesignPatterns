af85d7362bebe41f4d6789a4e9d130857d5bc580
JesusRRr <rolonjesus1996@gmail.com>
2021-02-10 20:27:06 -0600

    chore: implement update method in ChannelSuscriber class from Observer

--- src/com/rolon/behavioral/observer/ChannelSubscriber.java
+++ src/com/rolon/behavioral/observer/ChannelSubscriber.java
@@ -1,8 +1,17 @@
 package com.rolon.behavioral.observer;
 
 public class ChannelSubscriber implements Observer{
+    private String name;
+
+    ChannelSubscriber(String name){
+        this.name=name;
+    }
     @Override
     public void update() {
-
+        String message = new StringBuilder("Hi ")
+                .append(name)
+                .append(" a new video was upload")
+                .toString();
+        System.out.println(message);
     }
 }
