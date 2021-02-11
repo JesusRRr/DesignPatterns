d69edd27a3de24d670ccee5b0d39de4b25aa8543
JesusRRr <rolonjesus1996@gmail.com>
2021-02-10 20:14:08 -0600

    chore: YoutubeChannel and ChannelSuscriber classes created

---
+++ src/com/rolon/behavioral/observer/ChannelSuscriber.java
@@ -0,0 +1,8 @@
+package com.rolon.behavioral.observer;
+
+public class ChannelSuscriber implements Observer{
+    @Override
+    public void update() {
+
+    }
+}
---
+++ src/com/rolon/behavioral/observer/YoutubeChannel.java
@@ -0,0 +1,18 @@
+package com.rolon.behavioral.observer;
+
+public class YoutubeChannel implements Observable{
+    @Override
+    public void attach(Observer o) {
+
+    }
+
+    @Override
+    public void detach(Observer o) {
+
+    }
+
+    @Override
+    public void notifyObserver() {
+
+    }
+}
