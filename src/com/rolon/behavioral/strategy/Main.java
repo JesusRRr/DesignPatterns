897b5babde9c19027120de7531bc020e6ad933ef
JesusRRr <rolonjesus1996@gmail.com>
2021-02-11 13:54:38 -0600

    chore: Purchase Class created

--- src/com/rolon/behavioral/strategy/Main.java
+++ src/com/rolon/behavioral/strategy/Main.java
@@ -3,5 +3,13 @@ package com.rolon.behavioral.strategy;
 public class Main {
     public static  void main(String[] args){
         System.out.println("Strategy Design Pattern");
+        Purchase p1 = new Purchase("Boots");
+        Purchase p2 = new Purchase("Pants");
+
+        FedexStrategy fs = new FedexStrategy();
+        DHLStrategy dhls = new DHLStrategy();
+
+
+
     }
 }
---
+++ src/com/rolon/behavioral/strategy/Purchase.java
@@ -0,0 +1,34 @@
+package com.rolon.behavioral.strategy;
+
+public class Purchase {
+    private String product;
+    private PaymentStrategy payment;
+    private ShipmentStrategy shipment;
+
+    Purchase(String product){
+        this.product=product;
+    }
+
+    public void setPayment(PaymentStrategy payment) {
+        this.payment = payment;
+    }
+
+    public void setShipment(ShipmentStrategy shipment) {
+        this.shipment = shipment;
+    }
+
+    public void getFinalPrice(){
+        String message = new StringBuilder()
+                .append("The total cost is: ")
+                .append(product)
+                .append("cost")
+                .append("+")
+                .append( this.shipment.calculateTaxes())
+                .toString();
+        System.out.println(message);
+    }
+
+    public void pay(){
+        payment.pay();
+    }
+}
