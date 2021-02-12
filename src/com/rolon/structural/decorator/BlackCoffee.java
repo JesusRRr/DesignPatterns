7aabad5704ed45268f83dab15c853f10ede4aaa4
JesusRRr <rolonjesus1996@gmail.com>
2021-02-12 14:11:05 -0600

    fix: BlackCoffee class was deleted

--- src/com/rolon/structural/decorator/BlackCoffee.java
+++
@@ -1,9 +0,0 @@
-package com.rolon.structural.decorator;
-
-public class BlackCoffee implements Drink {
-    @Override
-    public String make() {
-        String message="Making a Black Coffee";
-        return message;
-    }
-}
